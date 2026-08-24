900c5675aafe76eee6b119ee0cc1e2467bf82098
akashsinh-09 <akashsodha81@gmail.com>
2026-08-24 20:52:06 +0530

    Conditonal statement tasks

---
+++ Controll statement & loop/task3.py
@@ -0,0 +1,6 @@
+num = int(input("Please enter a number: "))
+
+if (num % 2 == 0) and (num % 3 == 0):
+    print("The number is even and divisible by 3.")
+else :
+    print("The number is not even and divisible by 3.")
